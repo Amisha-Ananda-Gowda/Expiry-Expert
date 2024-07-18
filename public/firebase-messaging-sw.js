bd2db8bd82311f6c7ae180e92ab94d1c73e00a2b
Amisha <21j03.amisha@sjec.ac.in>
2024-07-19 00:27:52 +0530

    testing push notification

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,10 +1,7 @@
-// Give the service worker access to Firebase Messaging.
-// Note that you can only use Firebase Messaging here. Other Firebase libraries are not available in the service worker.
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
 
 // Initialize the Firebase app in the service worker by passing in your app's Firebase config object.
-// https://firebase.google.com/docs/web/setup#config-object
 const firebaseConfig = {
     apiKey: "AIzaSyAiJHDxxRHQ908Giz98Cj-smsZA7afcZqs",
     authDomain: "expiry-expert.firebaseapp.com",
@@ -15,19 +12,16 @@ const firebaseConfig = {
     measurementId: "G-Y52115VP3Y"
 };
 
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-// Retrieve an instance of Firebase Messaging so that it can handle background messages.
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
     console.log('[firebase-messaging-sw.js] Received background message ', payload);
-    // Customize notification here
     const notificationTitle = payload.notification.title;
     const notificationOptions = {
         body: payload.notification.body,
-        icon: payload.notification.image,
+        icon: payload.notification.image
     };
 
     self.registration.showNotification(notificationTitle, notificationOptions);
--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,14 @@
 import { initializeApp } from "firebase/app";
-import { getMessaging , getToken} from "firebase/messaging";
-
+import { getMessaging, getToken } from "firebase/messaging";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAiJHDxxRHQ908Giz98Cj-smsZA7afcZqs",
-  authDomain: "expiry-expert.firebaseapp.com",
-  projectId: "expiry-expert",
-  storageBucket: "expiry-expert.appspot.com",
-  messagingSenderId: "238047407921",
-  appId: "1:238047407921:web:d0ac6a979708fb32464a10",
-  measurementId: "G-Y52115VP3Y"
+    apiKey: "AIzaSyAiJHDxxRHQ908Giz98Cj-smsZA7afcZqs",
+    authDomain: "expiry-expert.firebaseapp.com",
+    projectId: "expiry-expert",
+    storageBucket: "expiry-expert.appspot.com",
+    messagingSenderId: "238047407921",
+    appId: "1:238047407921:web:d0ac6a979708fb32464a10",
+    measurementId: "G-Y52115VP3Y"
 };
 
 // Initialize Firebase
@@ -17,20 +16,18 @@ const app = initializeApp(firebaseConfig);
 export const messaging = getMessaging(app);
 
 export const generateToken = async () => {
-  
-    const permission = await Notification.requestPermission();
-    if (permission === "granted") {
-    const token=await getToken(messaging, {
-      vapidKey:
-      "BI0zo9B0496L0zk-JMxtryrDbI0VkNHEZ1A9dYJ-KUnOArMKPHjlpfHJiq22npcLPcaeYr0q6xmlDsV_9tz23cE",
-    });
-console.log(token);
-   
-        console.log("Permission granted");
-        // Proceed with setting up notifications
-    } else {
-        console.log("Permission not granted");
+    try {
+        const permission = await Notification.requestPermission();
+        if (permission === "granted") {
+            const token = await getToken(messaging, {
+                vapidKey: "BI0zo9B0496L0zk-JMxtryrDbI0VkNHEZ1A9dYJ-KUnOArMKPHjlpfHJiq22npcLPcaeYr0q6xmlDsV_9tz23cE",
+            });
+            console.log('Token:', token);
+            // Send the token to your server and store it in a database for later use
+        } else {
+            console.error("Permission not granted for notifications.");
+        }
+    } catch (error) {
+        console.error("An error occurred while retrieving token. ", error);
     }
-
 };
-
